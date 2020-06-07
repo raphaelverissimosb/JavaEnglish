01f9b9637e1f3c76579ae079fd7c9acc642700b6
Raphael Veríssimo <62971242+raphaelverissimosb@users.noreply.github.com>
2020-06-06 22:09:25 -0300

    Primitive variables

---
+++ ByteShotIntLong/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myValue = 10000;
+
+	    int myMinInValue = Integer.MIN_VALUE;
+	    int myMaxIntValue = Integer.MAX_VALUE;
+    }
+}
--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,16 @@
 public class Hello {
     public static void main(String[] args){
        System.out.print("Raphael");
+
+       int myFirstNumber = 5;
+       int myThirdNUmber = 6;
+       int mySecondNumber = myFirstNumber * 3;
+       int myTotal = myFirstNumber + mySecondNumber + myThirdNUmber;
+
+       System.out.println(myTotal);
+
+       int myLastOne = 500 - myTotal;
+       System.out.println(myLastOne);
+
     }
 }
